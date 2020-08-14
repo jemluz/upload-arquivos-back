e57f24ff3e425b7a9eae75c89d4240cc91f38180
Jemima <contatojemima@gmail.com>
2020-08-14 10:16:19 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
+const Post = require('./models/Posts')
+
+routes.get('/posts', async (req, res) => {
+  const posts = await Post.find();
+  return res.json(posts);
+})
+
+routes.post('/posts', multer(multerConfig).single('file'), async(req, res) => {
+  const { originalname: name, size, key, location: url = '' } = req.file
+  const post = await Post.create({ name, size, key, url });
+  return res.json(post);
+})
+
+routes.delete('/posts/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  await post.remove();
+  return res.status(200).json({ msg: "Imagem apagada." });
+})
+
+module.exports = routes;
